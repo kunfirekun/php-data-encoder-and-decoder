e2c6dbc42c99f48b8ca52907bed40f40d33c897c
kunfirekun <okello_daniel@hotmail.com>
2022-06-21 13:14:33 +0300

    encode test 1 with user defined value

--- encode-decode-master.php
+++ encode-decode-master.php
@@ -1,3 +1,6 @@
+
+<?php 
+$page='12345'; // user defined value
 $privateKey1 	= 'DANFOVBWEVCB432HNFC032FH2QADJ'; // user define key
     $secretKey1		= 'g124hbfo02fg'; // user define secret key
     $encryptMethod1      = "AES-256-CBC";
@@ -7,3 +10,7 @@ $privateKey1 	= 'DANFOVBWEVCB432HNFC032FH2QADJ'; // user define key
     $ivalue1 = substr(hash('sha256', $secretKey1), 0, 16); // sha256 is hash_hmac_algo
     $result1 = openssl_encrypt($string1, $encryptMethod1, $key1, 0, $ivalue1);
     $output1= base64_encode($result1);  // output is a encripted value
+
+    echo"$output1";
+
+    ?>
