1bf9b94a01760f7d8bdd2fa3d6db91ee79398f84
kunfirekun <okello_daniel@hotmail.com>
2022-06-21 13:44:02 +0300

    decode output 1 and output 2 test

--- decode-encoded-master.php
+++ decode-encoded-master.php
@@ -45,4 +45,18 @@ $ivalue = substr(hash('sha256', $secretKey), 0, 16); // sha256 is hash_hmac_algo
 
 echo"output 1 decoded: $output_decoded"; //output1 decoded succesfully
 
+
+//decrypt code for output2
+$privateKey2	= 'DANFOVBWEVCB432HNFC032FH2QADJ'; // user define key
+$secretKey2 		= 'g124hbfo02fg'; // user define secret key
+$encryptMethod2      = "AES-256-CBC";
+$stringEncrypt2      = $output2; // user encrypt value
+
+$key2    = hash('sha256', $privateKey2);
+$ivalue2 = substr(hash('sha256', $secretKey2), 0, 16); // sha256 is hash_hmac_algo
+
+ $output_decoded2 = openssl_decrypt(base64_decode($stringEncrypt2), $encryptMethod2, $key2, 0, $ivalue2);
+
+echo"output 1 decoded: $output_decoded2"; //output1 decoded succesfully
+
     ?>
